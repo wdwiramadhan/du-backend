34bda7e4f8a83ae52a5f4512122d3ed19bc7705a
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-02-24 22:02:26 +0700

    change router

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const routes = require("./routes/index.js");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(require("./routes/index.js"));
+app.use(routes);
 
 module.exports = app;
