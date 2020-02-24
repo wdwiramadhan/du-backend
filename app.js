c0d264b97555c3a3b3874114406731d044f36c7e
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-02-25 06:59:31 +0700

    new const for router

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require('cors');
 const routes = require("./routes/index.js");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(routes);
 
