41555b284d7de2adde14714c5af2200acd69596a
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-02-25 07:03:25 +0700

    change controller directory

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const user = require("../controllers/user");
 
-router.get("/", user.getAll);
-router.post("/", user.create);
-router.get("/:id", user.getById);
+router.get("/", user.index);
+router.post("/", user.store);
+router.get("/:id", user.show);
 router.put("/:id", user.update);
-router.delete("/:id", user.delete);
+router.delete("/:id", user.destroy);
 
 module.exports = router;
