493ef6176d7d7a66ec170e09eb8126ee88b3d8a6
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-02-27 15:31:38 +0700

    change routes

--- routes/registrant.js
+++ routes/registrant.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const registrant = require("../controllers/registrant");
 const { authenticate } = require("../middlewares/auth");
 
+router.get("/", registrant.index);
 router.post("/", registrant.store);
 router.get("/:id", authenticate, registrant.show);
 router.put("/:id", authenticate, registrant.update);
