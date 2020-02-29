583898f92689b3ee07a6f6a032861f7d32231c71
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-02-29 17:04:21 +0700

    integrate to schedule routes

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const auth = require("./auth");
 const user = require("./user");
+const schedule = require("./schedule");
 const { authenticate } = require("../middlewares/auth");
 
 app.get("/", (req, res) => {
   res.json({ message: "welcome" });
 });
 app.use("/auth", auth);
-app.use("/user",authenticate, user);
+app.use("/user", authenticate, user);
+app.use("/schedule", schedule);
 
 module.exports = app;
