7d18ca1026a2c73bb2f2b0f0085e28f522863e95
haqqer <ilhamserbaguna@gmail.com>
2020-03-13 13:30:27 +0700

    add auth middleware for schedule

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const schedule = require("../controllers/schedule");
+const { authenticate } = require("../middlewares/auth");
 
 router.get("/", schedule.index);
-router.post("/", schedule.store);
+router.post("/", authenticate, schedule.store);
 router.get("/:id", schedule.show);
-router.put("/:id", schedule.update);
-router.delete("/:id", schedule.destroy);
+router.put("/:id", authenticate, schedule.update);
+router.delete("/:id", authenticate, schedule.destroy);
 
 module.exports  = router
